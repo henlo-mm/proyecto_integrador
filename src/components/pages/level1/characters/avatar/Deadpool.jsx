7bf198e63df30123e96c92fabac1b7c1f1071b8f
henlo-mm <esperanza.olivo@correounivalle.edu.co>
2024-05-06 12:39:39 -0500

    Se reducen geometrias de modelos

--- src/components/pages/level1/characters/avatar/Deadpool.jsx
+++ src/components/pages/level1/characters/avatar/Deadpool.jsx
@@ -6,7 +6,7 @@ import { useThree } from "@react-three/fiber";
 import * as THREE from "three";
 import React, { forwardRef } from "react";
 
-export default function Deadpool({onCollision}) {
+export default function Deadpool({ onCollision }) {
 	const avatarRef = useRef();
 	const rigidBodyAvatarRef = useRef();
 	const { avatar, setAvatar } = useAvatar();
@@ -16,28 +16,31 @@ export default function Deadpool({onCollision}) {
 	const { gl } = useThree();
 
 	const { actions } = useAnimations(animations, avatarRef)
-	const shootSound = useMemo(() => new Audio("/assets/sounds/shoot.mp3"), []);
+	const shootSound = useMemo(() => {
+		const audio = new Audio("/assets/sounds/shoot.mp3");
+		audio.load(); 
+		return audio;
+	}, []);
+	
 
-    const onRightClick = useCallback((event) => {
-        event.preventDefault();
-        if (actions.Shooting) {
-            setAvatar({ ...avatar, animation: "Shooting" });
-        }
-        actions.Shooting.reset().fadeIn(0.05).setLoop(THREE.LoopOnce).play();
+	const onRightClick = useCallback((event) => {
+		event.preventDefault();
+		if (actions.Shooting) {
+			setAvatar({ ...avatar, animation: "Shooting" });
+		}
+		actions.Shooting.reset().fadeIn(0.05).setLoop(THREE.LoopOnce).play();
 
-        if (!shootSound.paused) {
-            shootSound.pause();
-            shootSound.currentTime = 0;
-        }
-        shootSound.play();
-    }, [actions, avatar, setAvatar, shootSound]);
+		shootSound.pause();
+		shootSound.currentTime = 0; 
+		shootSound.play();
+	}, [actions, avatar, setAvatar, shootSound]);
 
-    useEffect(() => {
-        gl.domElement.addEventListener('contextmenu', onRightClick);
-        return () => {
-            gl.domElement.removeEventListener('contextmenu', onRightClick);
-        };
-    }, [gl.domElement, onRightClick]);
+	useEffect(() => {
+		gl.domElement.addEventListener('contextmenu', onRightClick);
+		return () => {
+			gl.domElement.removeEventListener('contextmenu', onRightClick);
+		};
+	}, [gl.domElement, onRightClick]);
 
 
 	useEffect(() => {
@@ -70,8 +73,8 @@ export default function Deadpool({onCollision}) {
 			characterInitDir={170}
 			camInitDir={{ x: 0, y: 10 }}
 			floatHeight={0}
-            animated
-            followLight
+			animated
+			followLight
 			userData={{ name: "Deadpool" }}
 		>
 
@@ -82,96 +85,66 @@ export default function Deadpool({onCollision}) {
 						geometry={nodes.EyeLeft.geometry}
 						material={materials.Wolf3D_Eye}
 						skeleton={nodes.EyeLeft.skeleton}
-						morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
-						morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="EyeRight"
 						geometry={nodes.EyeRight.geometry}
 						material={materials.Wolf3D_Eye}
 						skeleton={nodes.EyeRight.skeleton}
-						morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
-						morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Body"
 						geometry={nodes.Wolf3D_Body.geometry}
 						material={materials.Wolf3D_Body}
 						skeleton={nodes.Wolf3D_Body.skeleton}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Facewear"
 						geometry={nodes.Wolf3D_Facewear.geometry}
 						material={materials.Wolf3D_Facewear}
 						skeleton={nodes.Wolf3D_Facewear.skeleton}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Glasses"
 						geometry={nodes.Wolf3D_Glasses.geometry}
 						material={materials.Wolf3D_Glasses}
 						skeleton={nodes.Wolf3D_Glasses.skeleton}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Head"
 						geometry={nodes.Wolf3D_Head.geometry}
 						material={materials.Wolf3D_Skin}
 						skeleton={nodes.Wolf3D_Head.skeleton}
-						morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
-						morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Headwear"
 						geometry={nodes.Wolf3D_Headwear.geometry}
 						material={materials.Wolf3D_Headwear}
 						skeleton={nodes.Wolf3D_Headwear.skeleton}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Outfit_Bottom"
 						geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
 						material={materials.Wolf3D_Outfit_Bottom}
 						skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Outfit_Footwear"
 						geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
 						material={materials.Wolf3D_Outfit_Footwear}
 						skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Outfit_Top"
 						geometry={nodes.Wolf3D_Outfit_Top.geometry}
 						material={materials.Wolf3D_Outfit_Top}
 						skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-						receiveShadow
-						castShadow
 					/>
 					<skinnedMesh
 						name="Wolf3D_Teeth"
 						geometry={nodes.Wolf3D_Teeth.geometry}
 						material={materials.Wolf3D_Teeth}
 						skeleton={nodes.Wolf3D_Teeth.skeleton}
-						morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
-						morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
-						receiveShadow
-						castShadow
 					/>
 					<primitive object={nodes.Hips} />
 				</group>
--- src/components/pages/level1/characters/enemies/Juggernaut.jsx
+++ src/components/pages/level1/characters/enemies/Juggernaut.jsx
@@ -68,30 +68,24 @@ export default function Juggernaut({ onCollision  }) {
             <CuboidCollider args={[-0.4, 0.5, -0.2]} position={[0, 1, 0]} /> 
 
             <group  ref={avatarRef} >
-                <group name="Armature"  >
+                <group name="Armature">
                     <skinnedMesh
                         name="EyeLeft"
                         geometry={nodes.EyeLeft.geometry}
                         material={materials.Wolf3D_Eye}
                         skeleton={nodes.EyeLeft.skeleton}
-                        morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
-                        morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
                     />
                     <skinnedMesh
                         name="EyeRight"
                         geometry={nodes.EyeRight.geometry}
                         material={materials.Wolf3D_Eye}
                         skeleton={nodes.EyeRight.skeleton}
-                        morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
-                        morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
                     />
                     <skinnedMesh
                         name="Wolf3D_Head"
                         geometry={nodes.Wolf3D_Head.geometry}
                         material={materials.Wolf3D_Skin}
                         skeleton={nodes.Wolf3D_Head.skeleton}
-                        morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
-                        morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
                     />
                     <skinnedMesh
                         name="Wolf3D_Outfit_Bottom"
@@ -116,8 +110,6 @@ export default function Juggernaut({ onCollision  }) {
                         geometry={nodes.Wolf3D_Teeth.geometry}
                         material={materials.Wolf3D_Teeth}
                         skeleton={nodes.Wolf3D_Teeth.skeleton}
-                        morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
-                        morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
                     />
                     <primitive object={nodes.Hips} />
                 </group>
